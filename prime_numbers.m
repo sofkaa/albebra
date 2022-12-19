58b2f991810aff646c0e1a8241583f8205cf5ddd
sofkaa <105396335+sofkaa@users.noreply.github.com>
2022-12-19 19:54:30 +0300

    Update prime_numbers.m

--- prime_numbers.m
+++ prime_numbers.m
@@ -10,86 +10,61 @@ function prime_numbers %main function
   k = randi([5 20]);
 
   arr(1) = 2;
-  deg(1) = 10;
-  el = 2;
+  deg(1) = 20;
 
-  num = 0;
-
-  for i = 2 : k
-    arr(i)=p(randi([1 31]));
-    if (arr(i) > el)
-      el = arr(i);
-    endif
-    deg(i)=randi([18 24]);
+  for i= 2 : k
+    arr(i) = p(randi([1 32]));
+    deg(i) = randi([18 25]);
   endfor
 
-  n = sym (arr(1))^(deg(1));
-  num = (arr(1))^(deg(1));
+  el = max(arr);
 
-  for i = 2 : k
-    n = n + sym(arr(i))^(deg(i));
-    num =  num + (arr(i))^(deg(i));
+  for i = 1 : k
+    n = n + arr(i)^deg(i);
   endfor
 
-  while (num < 2^123)
-    n = sym (ceil(n * el));
-    num = ceil(num * el);
-  endwhile
-  while (num > 2^128)
-    n = sym (floor(n / sym(el)));
-    num = floor(num / el);
-  endwhile
-
-  n = n + 1;
-  num % вывод числа
-
-  flag = 0;
-  N = ceil(log2(num));
-  for a = 2 : N
-    for i = 1 : k
-      t = a^((num-1) / p(i));
-      if (mod(t, num) != 1)
-        if (mod(a^(num-1), num) == 1)
-          flag = 1;
-        endif
-      endif
+  while((n > 2^128) && (n < 2^123))
+    k = randi([5 10]);
+    arr(1) = 2;
+    deg(1) = 20;
+    for i= 2 : k
+      arr(i) = p(randi([1 32]));
+     deg(i) = randi([18 25]);
     endfor
-  endfor
-  disp(sym(n));
+    el = max(arr);
+    n = 0;
+    for i = 1 : k
+     n = n + arr(i)^deg(i);
+    endfor
+    log2(n);
+  endwhile
 
-  if(flag == 0)
-    disp("является простым");
-  else
-   disp("НЕ простое");
-  end
+  n = n+1;
+  answer = 0;
 
-  [k1, k1um] = makenum(p, k) %как множитель p
-  [k2, k2um] = makenum(p, k) %как множитель q
-  n = k1 * k2 % n = pq
-  num = k1um * k2um
-
-  flag = 0;
-  aum = floor(num / 2);
-  a = (n-1) / 2; k = 1;
-  N = ceil(log2(num));
-  for i = 1 : N
-    k = k * i;
-    b = mod(sym(a)^(k) - 1, n);
-    bum = mod(aum^k - 1, num);
-
-    t = evklid(bum, num);
-    if ((t > 1) && (t < num))
-      flag = 1;
+  for a = 2 : ceil(log2(n))
+    if (mod(a^(n-1), n) == 1)
+      ans = 1;
+      for i = 1 : k
+        if (mod(a^((n-1) / p(i)), n) == 1)
+          ans = 0;
+        endif
+      endfor
+      if (ans == 1)
+        answer = a;
+      endif
     endif
   endfor
-  if (flag == 0)
-    disp(1);
-  else
-    disp(sym(b));
-    bum
-  endif
 
-end
+  if (answer != 0)
+    disp(sym(n));
+    disp("простое");
+  else
+   disp(sym(n));
+   disp("НЕ простое");
+  endif
+  pollard_alg(n, round(log2(n)));
+endfunction
 
 function arr = simple_list (N) % ищем простые числа в диапазоне 2 - 128
   arr = zeros(N);
@@ -99,41 +74,25 @@ function arr = simple_list (N) % ищем простые числа в диап�
       break
     endif
   endfor
-  if (j > (k/j))
+  if (j > (k / j))
     arr(j) = k;
   endif
   endfor
-  arr(arr==0)=[];
+  arr(arr == 0) = [];
 endfunction
 
-function [n, num] = makenum(p,k)
-  arr(1) = 2;
-  deg(1) = 10;
-  el = 2;
-  for i = 2 : k
-    arr(i) = p(randi([1 31]));
-    if (arr(i) > el)
-      el = arr(i);
+function pollard_alg(n, N)
+  res = 0;
+  a = floor(n / 2);
+  for i = 1 : N
+    b = mod(a^factorial(i)-1, n);
+    if ((gcd(b, n) != 1) && (gcd(b, n) != n))
+      res = 1;
+      sym(b)
     endif
-    deg(i) = randi([5 10]);
   endfor
-  n = sym (arr(1))^(deg(1));
-  num = (arr(1))^(deg(1));
-  for i = 2 : k
-    n = n + sym(arr(i))^(deg(i));
-    num =  num + (arr(i))^(deg(i));
-  endfor
-endfunction
-
-function z = evklid(a,b)
-	c = a - b;
-	if (b > c) evklid(b, c);
+  if(res == 0)
+    1
   endif
-	if (c > b) evklid(c, b);
-	endif
-	if (c == b)
-	  nod = c;
-	  z = nod;
-	endif
 endfunction
 
